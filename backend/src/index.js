fbc2a9f14ebbce3d64429848f044b1514168daa4
Vinicius Moralis <vinicius_moralis@hotmail.com>
2020-01-27 15:02:55 -0300

    criando cadastro/visualizacao de dados do bd

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors')
 
 const app = express();
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://admin:123@cluster0-shard-00-00-s1tll.mongodb.net:270
 
 mongoose.set('useCreateIndex', true)
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
