8854f786506c2b37ad7e171773e58c08f34a3227
Vinicius Moralis <vinicius_moralis@hotmail.com>
2020-02-16 14:26:07 -0300

    aula 04: inicializando websocket no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-const cors = require('cors')
+const cors = require('cors');
+const http = require('http');
+const { setWebSocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setWebSocket(server);
 
 mongoose.connect('mongodb://admin:123@cluster0-shard-00-00-s1tll.mongodb.net:27017,cluster0-shard-00-01-s1tll.mongodb.net:27017,cluster0-shard-00-02-s1tll.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-
 mongoose.set('useCreateIndex', true)
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(2222);
+server.listen(2222);
